f47da0f9296203e4a431ec0c3675196af72df720
kat geo <katie.georgieva@gmail.com>
2019-11-19 22:15:29 +0200

    tests ready

--- api-tests/login.test.js
+++ api-tests/login.test.js
@@ -1,15 +1,28 @@
 import credentials from "../fixtures/credentials";
 import request from "../utils/request";
 
-test("/api/login with correct credentials", async () => {
+
+test("/api/login with incorrect credentials", async () => {
+
   let response = await request({
     method: "POST",
     url: "https://sprinkle-burn.glitch.me/api/login",
     body: credentials.invalid
   });
-  fail("Not yet implemented");
+
+  expect(response.statusCode).toEqual(401);
+
 });
 
-test("/api/login correct credentials", async () => {
-  fail("Not yet implemented");
+test("/api/login with correct credentials", async () => {
+  
+   let response = await request({
+    method: "POST",
+    url: "https://sprinkle-burn.glitch.me/api/login",
+    body: credentials.valid
+  });
+
+  expect(response.statusCode).toEqual(200);
+
 });
+
--- ui-tests/login.js
+++ ui-tests/login.js
@@ -1,12 +1,38 @@
 import { Selector } from "testcafe";
 import credentials from "../fixtures/credentials";
 
-fixture`Login Page`.page`https://sprinkle-burn.glitch.me/`;
+fixture ('Login Page').page('https://sprinkle-burn.glitch.me/');
 
-test("Can not login with correct credentials", () => {
-  throw new Error("Test not implemented");
+test("Can not login with correct credentials", async t => {
+  
+        const emailBox = Selector('input').withAttribute('type', 'email');
+        const passwordBox = Selector('input').withAttribute('type', 'password');
+        const loginBtn = Selector('button').withText('Login');
+
+          await t
+            .typeText(emailBox, 'jsnow@drugdev.com')
+            .typeText(passwordBox, 'winteriscoming')
+            .click(loginBtn)
+
+        // Use the assertion to check if the actual  text is equal to the expected one
+        .expect(Selector('#login-error-box').innerText).eql('Credentials are incorrect');
 });
 
-test("Can login with correct credentials", () => {
-  throw new Error("Test not implemented");
+test("Can login with correct credentials", async t => {
+      
+        const emailBox = Selector('input').withAttribute('type', 'email');
+        const passwordBox = Selector('input').withAttribute('type', 'password');
+        const loginBtn = Selector('button').withText('Login');
+        const welcome = Selector('article').withText('Welcome Dr I Test');
+
+          await t
+          .typeText(emailBox, 'test@drugdev.com')
+          .typeText(passwordBox, 'supers3cret')
+          .click(loginBtn)
+
+        // Use the assertion to check if the actual text is equal to the expected one
+        .expect(welcome.textContent).contains('Welcome Dr I Test');
+
 });
+
+
