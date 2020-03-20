42a7e0fd81c48a6d1f1d17f204b2243ab296d70a
kat geo <katie.georgieva@gmail.com>
2020-03-20 23:54:03 +0200

    reafactor

--- ui-tests/login.js
+++ ui-tests/login.js
@@ -1,6 +1,6 @@
 import { Selector } from "testcafe";
 import credentials from "../fixtures/credentials";
-import loginpage from "../ui-tests/page-obejctas/loginpage";
+import loginpage from "./page-objects/loginpage";
 
 fixture ('Login Page').page('https://sprinkle-burn.glitch.me/');
 
@@ -12,18 +12,15 @@ test("Can not login with correct credentials", async t => {
             .click(loginpage.loginBtn)
 
         // Use the assertion to check if the actual  text is equal to the expected one
-        .expect(loginpage.ermsg.textContent).eql('Credentials are incorrect');
+        .expect(loginpage.ermsg.textContent).contains('Credentials are incorrect');
 });
 
 test("Can login with correct credentials", async t => {
       
-        
-       // const welcome = Selector('article').withText('Welcome Dr I Test');
-
           await t
-          .typeText(loginpage.emailBox, 'test@drugdev.com')
-          .typeText(loginpage.passwordBox, 'supers3cret')
-          .click(loginpage.loginBtn)
+            .typeText(loginpage.emailBox, 'test@drugdev.com')
+            .typeText(loginpage.passwordBox, 'supers3cret')
+            .click(loginpage.loginBtn)
 
         // Use the assertion to check if the actual text is equal to the expected one
         .expect(loginpage.wlcm.textContent).contains('Welcome Dr I Test');
@@ -32,12 +29,11 @@ test("Can login with correct credentials", async t => {
 
 test("Cannot login with blank credentials", async t => {
   
-    await t
-      
-      .click(loginpage.loginBtn)
+          await t
+          .click(loginpage.loginBtn)
 
   // Use the assertion to check if the actual  text is equal to the expected one
-  .expect(loginpage.ermsg.textContent).eql('Credentials are incorrect');
+      .expect(loginpage.ermsg.textContent).eql('Credentials are incorrect');
 });
 
 
